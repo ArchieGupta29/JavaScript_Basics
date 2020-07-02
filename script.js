596ee32191c46ffddb32d01b5cc18fccb4ad3c5e
archiegupta <archiegupta99@gmail.com>
2020-07-02 18:56:20 +0530

    Third commit

--- script.js
+++ script.js
@@ -184,3 +184,15 @@ function calculateAge(birthYear) {
 
 var ageJohn = calculateAge(1990);
 console.log(ageJohn);
+
+function yearsUntilRetirement(year, firstName){
+    var age = calculateAge(year);
+    var retirement = 65 - age;
+    if(retirement>0){
+      console.log(firstName + 'retires in'+retirement+'years');  
+    } else {
+        console.log(firstName + 'is already retired');
+    }
+}
+
+yearsUntilRetirement(1990, 'john');
